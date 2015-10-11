1f82b545f526f1c84008fdb507653cc355abba5c
Jennifer Patel <jennifer@goinvo.com>
2015-10-10 20:02:17 -0400

    lined up APIs

--- assets/js/controllers/appController.js
+++ assets/js/controllers/appController.js
@@ -1,3 +1,4 @@
 app.controller('appController', ['$scope', function($scope){
 	$scope.title = 'Jen Plays Games';
+	$scope.games = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=sE1236C804C048AFC127EE0003D297864&steamid=76561197960434622&format=json';
 }]);
